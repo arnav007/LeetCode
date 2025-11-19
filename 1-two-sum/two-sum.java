8052dc145dbf48f28f1dfe090e0e5625917bf809
Arnav <arnav007.m@gmail.com>
2025-11-19 08:22:15 +0530

    two-sum

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,6 +1,10 @@
 class Solution {
+    // public static void main(String[] args) {
+    //     Solution s =new Solution();
+    //     s.twoSum(new int[]{1,2,3,4},5);
+    // }
     public int[] twoSum(int[] nums, int target) {
-        // for(int i=0;i<nums.length-1;i++){
+    // for(int i=0;i<nums.length-1;i++){
         //     for(int j=i+1;j<nums.length;j++){
         //         if((nums[i]+nums[j]) == target){
         //             return new int[]{i,j};
